89cd6af378aecb17634aef9f68430bfb86e392bb
Baumel <christianyonta73@gmail.com>
2026-04-17 13:53:54 +0100

    lab 0 submission

--- client.py
+++ client.py
@@ -1,25 +1,18 @@
-# CYBE 6223 — Lab 0 | server.py
-# Group Name:
-# Group Number:
-#
-# Member 1 — Name:                    Matric No.:
-# Member 2 — Name:                    Matric No.:
-# Member 3 — Name:                    Matric No.:
-# Member 4 — Name:                    Matric No.:
-# Member 5 — Name:                    Matric No.:
-# Member 6 — Name:                    Matric No.:
-# Member 7 — Name:                    Matric No.:
-# Member 8 — Name:                    Matric No.:
-#
-# TODO: Implement a TCP echo client
-# Requirements:
-#   - Connect to HOST = '127.0.0.1', PORT = 9000
-#   - Send a message of your choice
-#   - Receive the server's echo and print it
+
 
 import socket
 
 HOST = '127.0.0.1'
 PORT = 9000
 
-# Your implementation here
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((HOST, PORT))
+
+message = input("Type your message here: ")
+client.sendall(message.encode())
+
+data = client.recv(1024)
+print("Received from server:", data.decode())
+
+client.close()
--- server.py
+++ server.py
@@ -1,26 +1,14 @@
-# CYBE 6223 — Lab 0 | server.py
-# Group Name:
-# Group Number:
-#
-# Member 1 — Name:                    Matric No.:
-# Member 2 — Name:                    Matric No.:
-# Member 3 — Name:                    Matric No.:
-# Member 4 — Name:                    Matric No.:
-# Member 5 — Name:                    Matric No.:
-# Member 6 — Name:                    Matric No.:
-# Member 7 — Name:                    Matric No.:
-# Member 8 — Name:                    Matric No.:
-#
-# TODO: Implement a TCP echo server
-# Requirements:
-#   - Bind to HOST = '127.0.0.1', PORT = 9000
-#   - Listen for one client connection
-#   - Receive a message and echo it back
-#   - Print a confirmation message when a client connects
-
 import socket
-
 HOST = '127.0.0.1'
 PORT = 9000
-
-# Your implementation here
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen(1)
+print("Server listening...")
+conn, addr = server.accept()
+print("Connected by", addr)
+data = conn.recv(1024)
+print("Received:", data.decode())
+conn.sendall(data)
+conn.close()
+server.close()
