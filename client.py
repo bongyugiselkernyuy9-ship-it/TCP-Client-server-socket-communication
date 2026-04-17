ab37411a59709a3fb5173659193ca8428f8bc5b0
Your Name <you@example.com>
2026-04-17 15:42:14 +0100

    Lab 0 work restored after repository recovery

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((HOST, PORT))
 
-message = input("Type your message here: ")
+message = input("Enter your message here: ")
 client.sendall(message.encode())
 
 data = client.recv(1024)
